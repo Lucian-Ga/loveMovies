9c9ab57d3e3c3e3302e7cd943bdd6909545490b2
Lucian-Ga <96188392+Lucian-Ga@users.noreply.github.com>
2024-09-20 00:56:31 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
-//const { PORT = 5000 } = process.env;
-const PORT = process.env.PORT || 3000;
+const { PORT = 5001 } = process.env;
 
 const app = require("./app");
 const knex = require("./db/connection");
